669872746e7f6a9093439284883c2f44ba187759
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-03-14 00:50:16 -0600

    avance

--- controllers/productos.js
+++ controllers/productos.js
@@ -6,7 +6,9 @@ const productos = [
     { id: 4, nombre: 'Camara', descripcion: 'Camara con una perfecta resolucion', cantidad: 10, precio: 1000, imagen: 'producto4.jpg' },
     { id: 5, nombre: 'Maquillaje', descripcion: 'Maquillaje de varios colores ', cantidad: 15, precio: 150, imagen: 'producto5.jpg' },
     { id: 6, nombre: 'Maquillaje y labial', descripcion: 'Maquillaje de color rosa y labial color rojo', cantidad: 5, precio: 150, imagen: 'producto6.jpg' },
-    { id: 7, nombre: 'Control de xbox one', descripcion: 'control de xbox one color blanco con negro', cantidad: 30, precio: 500, imagen: 'producto7.jpg' }
+    { id: 7, nombre: 'Control de xbox one', descripcion: 'control de xbox one color blanco con negro', cantidad: 30, precio: 500, imagen: 'producto7.jpg' },
+    { id: 8, nombre: 'silla gamer', descripcion: 'silla gamer color rojo con negro', cantidad: 40, precio: 1500, imagen: 'producto8.jpg' },
+    { id: 9, nombre: 'computadora', descripcion: 'computadora huawei core i5 ', cantidad: 40, precio: 22500, imagen: 'producto9.jpg' }
   ];
   
   function getProductos() {
