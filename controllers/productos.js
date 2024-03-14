7fb052b254bd3011d6d66be9ba888c54d86e61a5
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-03-13 19:29:13 -0600

    avance

--- controllers/productos.js
+++ controllers/productos.js
@@ -1,7 +1,7 @@
 // controllers/productos.js
 const productos = [
-    { id: 1, nombre: 'Producto 1', descripcion: 'Descripción del Producto 1', cantidad: 10, precio: 100, imagen: 'producto1.jpg' },
-    { id: 2, nombre: 'Producto 2', descripcion: 'Descripción del Producto 2', cantidad: 15, precio: 150, imagen: 'producto2.jpg' },
+    { id: 1, nombre: 'Hamburgesa', descripcion: 'Hamburguesa de doble carne,lechuga,jitomate', cantidad: 15, precio: 50, imagen: 'producto1.jpg' },
+    { id: 2, nombre: 'Gorra', descripcion: 'Gorra de color negra de cuero muy resistente', cantidad: 20, precio: 120, imagen: 'producto2.jpg' },
     { id: 3, nombre: 'Producto 3', descripcion: 'Descripción del Producto 3', cantidad: 5, precio: 50, imagen: 'producto3.jpg' },
     { id: 4, nombre: 'Producto 4', descripcion: 'Descripción del Producto 4', cantidad: 10, precio: 100, imagen: 'producto4.jpg' },
     { id: 5, nombre: 'Producto 5', descripcion: 'Descripción del Producto 5', cantidad: 15, precio: 150, imagen: 'producto5.jpg' },
