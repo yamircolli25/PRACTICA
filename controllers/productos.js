be4fc725abbde6795e42f6ae3dfc1bdb41849ced
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-03-13 21:50:47 -0600

    Update productos.js

--- controllers/productos.js
+++ controllers/productos.js
@@ -6,8 +6,7 @@ const productos = [
     { id: 4, nombre: 'Camara', descripcion: 'Camara con una perfecta resolucion', cantidad: 10, precio: 1000, imagen: 'producto4.jpg' },
     { id: 5, nombre: 'Maquillaje', descripcion: 'Maquillaje de varios colores ', cantidad: 15, precio: 150, imagen: 'producto5.jpg' },
     { id: 6, nombre: 'Maquillaje y labial', descripcion: 'Maquillaje de color rosa y labial color rojo', cantidad: 5, precio: 150, imagen: 'producto6.jpg' },
-    { id: 7, nombre: 'Control de xbox one', descripcion: 'control de xbox one color blanco con negro', cantidad: 30, precio: 500, imagen: 'producto7.jpg' },
-    { id: 8, nombre: 'Control de', descripcion: 'control de xbox one color blanco con negro', cantidad: 30, precio: 500, imagen: 'producto8.jpg' }
+    { id: 7, nombre: 'Control de xbox one', descripcion: 'control de xbox one color blanco con negro', cantidad: 30, precio: 500, imagen: 'producto7.jpg' }
   ];
   
   function getProductos() {
